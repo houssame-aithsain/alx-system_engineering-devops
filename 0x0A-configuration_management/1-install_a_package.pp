369e9a4104c76d116df7d98d3a3386db09e78449
Houssame <houssame1337@gmail.com>
2024-06-28 21:25:30 -0400

    00

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,15 @@
 # this is a manifest file to install a package
+package { 'python3.8':
+    ensure   => '3.8.10',
+    provider => 'apt',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
+    provider => 'pip3',
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
     provider => 'pip3',
 }
