4fc82b0d933969cf4e102ea24f118510d4dcd228
Houssame <houssame1337@gmail.com>
2024-06-30 19:30:18 -0400

    00

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,6 @@ file_line { 'Turn off passwd auth':
     ensure => 'present',
     path  => '/etc/ssh/sshd_config',
     line  => 'PasswordAuthentication no',
-    match => '^#?PasswordAuthentication',
 }
 
 # Declare identity file
@@ -11,5 +10,4 @@ file_line { 'Declare identity file':
     ensure => 'present',
     path  => '/etc/ssh/ssh_config',
     line  => 'IdentityFile ~/.ssh/school',
-    match => '^#?IdentityFile',
 }
