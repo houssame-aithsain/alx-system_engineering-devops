8978301334d7dca9aa3d7fb7b6b50c1cf446759d
Houssame <houssame1337@gmail.com>
2024-07-02 22:56:05 -0400

    00

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,38 @@
-# Ensure Nginx is installed
-
+# Ensure the system package list is updated
 exec { 'update system':
-        command => '/usr/bin/apt-get update',
+  command     => '/usr/bin/apt-get update',
+  path        => ['/bin', '/usr/bin'],
+  before      => Package['nginx'],
+  refreshonly => true,
 }
 
+# Ensure Nginx is installed
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+  ensure => installed,
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+# Ensure the default page contains 'Hello World!'
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+# Configure Nginx to redirect /redirect_me
+file_line { 'nginx_redirect':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  match   => '^.*redirect_me.*$',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+# Ensure Nginx is running and enabled
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File['/var/www/html/index.html', '/etc/nginx/sites-available/default'],
 }
