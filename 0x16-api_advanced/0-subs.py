76a18cf8b92a120de14e769baf2249f798c691b4
Houssame <hossame@inbox.ru>
2024-08-13 15:58:05 +0100

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and returns the number of subscribers """
+"""0-subs.py"""
 import requests
 
 
-def number_of_subscribers(subreddit) -> int:
+def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers
-
-    Args:
-        subreddit (str): subreddit to query
-
-    Returns:
-        (int): number of subscribers
+    Queries the Reddit API and returns
+    the number of subscribers for a given subreddit.
     """
-    # Returns the number of subscribers for a given subreddit
-    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    try:
-        request = requests.get(base_url, headers={'User-Agent': 'Agent Uche'})
-        data = request.json()
-        return data['data']['subscribers']
-    except Exception:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'custom'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    return response.json()['data']['subscribers']
