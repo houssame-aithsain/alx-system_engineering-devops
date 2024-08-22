fd85b45c45d13d6d6eda5525f6456a978e7faf24
Houssame <hossame@inbox.ru>
2024-08-22 20:37:29 +0100

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,16 @@
-# Fixes an nginx site that can't handle multiple concurrent requests
-exec { 'fix--for-nginx':
-  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
-/etc/default/nginx; service nginx restart\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
+# This Puppet manifest fixes an issue with an Nginx site that can't handle multiple concurrent requests.
+# It does this by increasing the limit on the number of file descriptors that can be opened by the Nginx process.
+# The limit is set in the /etc/default/nginx file, and this manifest modifies that file to set the limit to 8192.
+# After modifying the file, it restarts the Nginx service to apply the changes.
+
+# Check if the file exists before trying to fix it
+if File.exists?('/etc/default/nginx') {
+  exec { 'fix-for-nginx':
+    command     => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
+    path        => '/usr/bin:/usr/sbin:/bin',
+    onlyif      => "bash -c \"grep -q '^ULIMIT=' /etc/default/nginx && ! grep -q '^ULIMIT=\\\"-n 8192\\\"' /etc/default/nginx\"",
+    logoutput   => true,
+  }
+} else {
+  notice('File /etc/default/nginx does not exist')
 }
