82c20551f29db4056d165f2fbf1ddee6f51f685a
Houssame <hossame@inbox.ru>
2024-08-22 20:39:08 +0100

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,14 +3,7 @@
 # The limit is set in the /etc/default/nginx file, and this manifest modifies that file to set the limit to 8192.
 # After modifying the file, it restarts the Nginx service to apply the changes.
 
-# Check if the file exists before trying to fix it
-if File.exists?('/etc/default/nginx') {
-  exec { 'fix-for-nginx':
-    command     => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
-    path        => '/usr/bin:/usr/sbin:/bin',
-    onlyif      => "bash -c \"grep -q '^ULIMIT=' /etc/default/nginx && ! grep -q '^ULIMIT=\\\"-n 8192\\\"' /etc/default/nginx\"",
-    logoutput   => true,
-  }
-} else {
-  notice('File /etc/default/nginx does not exist')
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
